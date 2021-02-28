e449e5c160b2b9f751908878c3b53b415c3f17f7
Fox_x <foxxx2game@gmail.com>
2021-02-28 15:04:31 +0200

    -

--- js/postget.js
+++ js/postget.js
@@ -1,15 +1,23 @@
-const SERVER_URL = "http://afppgmc.face-six.com:3010/fa6_api/";
+const SERVER_URL = "https://afppgmc.face-six.com:3011/fa6_api/";
 const TOKEN = "b0d037d2bfb311b51791bae981c41f6a";
 
 async function getNewTaskID() {
-  fetch(SERVER_URL + "processing", {
-    method: "POST",
-    headers: {
-		"Authorization": "b0d037d2bfb311b51791bae981c41f6a",
-		"Content-Type": "multipart/form-data"
-	},
-    redirect: "follow",
-  }).then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));;
+	var myHeaders = new Headers();
+	myHeaders.append("Authorization", "b0d037d2bfb311b51791bae981c41f6a");
+	myHeaders.append("Content-Type", "multipart/form-data");
+	
+	var formdata = new FormData();
+	formdata.append("POST", "{\"clientid\":3,\"locationid\":3, \"observations\":[]}");
+	
+	var requestOptions = {
+	  method: 'POST',
+	  headers: myHeaders,
+	  body: formdata,
+	  redirect: 'follow'
+	};
+	
+	fetch("https://afppgmc.face-six.com:3011/fa6_api/processing", requestOptions)
+	  .then(response => response.text())
+	  .then(result => console.log(result))
+	  .catch(error => console.log('error', error));
 }
