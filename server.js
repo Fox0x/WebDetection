45ababdc169ce1fb9214acba784e39eab18617f1
Fox_x <foxxx2game@gmail.com>
2021-02-28 13:15:31 +0200

    chahge server.js for heroku

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 'use strict';
 
-import express, { json, urlencoded, static } from 'express';
-import { join } from 'path';
+const express = require('express');
+const path = require('path')
 const app = express();
-app.use(json())
-app.use(urlencoded({extended: true}))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 const mainDir = join(__dirname, '/')
 app.use(static(mainDir));
 app.use(static(join(__dirname, './css')));
 app.use(static(join(__dirname, './js')));
-app.get('/', (req, res) => res.sendFile(join(mainDir, 'index.html')))
+app.get('/', (req, res) => res.sendFile(path.join(mainDir, 'index.html')));
 
 app.listen(process.env.PORT || 3001)
