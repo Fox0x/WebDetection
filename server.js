8ed13696db36f4a4b96d9ea24fa1f3774f0d02b5
Fox_x <foxxx2game@gmail.com>
2021-02-28 13:18:11 +0200

    +

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
-'use strict';
-
 const express = require('express');
 const path = require('path')
 const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-const mainDir = join(__dirname, '/')
-app.use(static(mainDir));
-app.use(static(join(__dirname, './css')));
-app.use(static(join(__dirname, './js')));
-app.get('/', (req, res) => res.sendFile(path.join(mainDir, 'index.html')));
+const mainDir = path.join(__dirname, '/')
+app.use(express.static(mainDir));
+app.use(express.static(path.join(__dirname, './css')));
+app.use(express.static(path.join(__dirname, './js')));
+app.get('/', (req, res) => res.sendFile(path.join(mainDir, 'index.html')))
 
 app.listen(process.env.PORT || 3001)
